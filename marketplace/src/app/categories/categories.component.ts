2a5de8e73090dd5ca248764bac32bcc7abf54469
jamesbondjb001 <jamesbondjb001@gmail.com>
2018-10-07 19:41:53 +0530

    13 commit

--- marketplace/src/app/categories/categories.component.ts
+++ marketplace/src/app/categories/categories.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output ,EventEmitter, Input} from '@angular/core';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  //styleUrls: ['./categories.component.scss']
+  styles: [
+    `
+      :host {
+        background: rgba(0, 0, 0, 0.1);
+      }
+
+      app-sticky-header {
+        padding-top: 150px;
+        
+        z-index: 10;
+      }
+
+      .content {
+        padding-left: 70px;
+      }
+
+    `
+  ]
 })
 export class CategoriesComponent implements OnInit {
+
+  // @Output() categorySel = new EventEmitter<String>();
+  @Input() categorySel ='home';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onCategorySelect(categorySelect : string){
+    console.log("cat " + categorySelect);
+    // this.categorySel.emit(categorySelect);
+    this.categorySel=categorySelect;
+  }
 }
